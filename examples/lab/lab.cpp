88487b1fc00080dee9bb835a4872d77d05eda380
mchodkiewicz <ml.chodkiewicz@uw.edu.pl>
2025-03-28 09:19:19 +0100

    some performance test in lab example added

--- examples/lab/lab.cpp
+++ examples/lab/lab.cpp
@@ -7,6 +7,7 @@
 #include "discamb/CrystalStructure/crystal_structure_utilities.h"
 #include "discamb/IO/discamb_io.h"
 #include "discamb/IO/fragmentation_io.h"
+#include "discamb/IO/hkl_io.h"
 #include "discamb/IO/structure_io.h"
 #include "discamb/IO/MATTS_BankReader.h"
 #include "discamb/IO/tsc_io.h"
@@ -598,9 +599,55 @@ void tsc_differences(
 
 }
 
+void calc_sf(
+    const string& structure_file,
+    const string& hkl_file)
+{
+    Crystal crystal;
+    WallClockTimer timer;
+    timer.start();
+    structure_io::read_structure(structure_file, crystal);
+    cout << "structure read in " << timer.stop() << " ms\n";
+
+
+    vector<Vector3i> hkl;
+
+    hkl_io::readHklIndices(hkl_file.c_str(), hkl);
+
+    vector<complex<double> > sf;
+    nlohmann::json json_data;
+    ifstream jsonFileStream("aspher.json");
+
+    if (jsonFileStream.good())
+        jsonFileStream >> json_data;
+    jsonFileStream.close();
+    timer.start();
+    auto sf_calculator = SfCalculator::create(crystal, json_data);
+    cout << "sf calculator created in " << timer.stop() << " ms\n";
+    vector<bool> countAtom(crystal.atoms.size(), true);
+    timer.start();
+    cout << "calculating sf for " << crystal.atoms.size() << " atoms and " << hkl.size() << " reflections\n";
+    sf_calculator->calculateStructureFactors(crystal.atoms, hkl, sf, countAtom);
+    cout << "sf calculated in " << timer.stop() << " ms\n";
+    ofstream out("cals_sf.txt");
+    for (int i = 0; i < hkl.size(); i++)
+        out << hkl[i] << " " << sf[i] << "\n";
+    out.close();
+    vector<TargetFunctionAtomicParamDerivatives> dTarget_dParam;
+    vector<complex<double> > dTarget_dF;
+    dTarget_dF.resize(hkl.size(),0.01);
+    cout << "calculating sf and derivatives\n";
+    timer.start();
+    sf_calculator->calculateStructureFactorsAndDerivatives(crystal.atoms, hkl, sf, dTarget_dParam, dTarget_dF, countAtom);
+    cout << "sf calculated in " << timer.stop() << " ms\n";
+}
+
 int main(int argc, char* argv[])
 {
     try { 
+        calc_sf(argv[1], argv[2]);
+        return 0;
+
         tsc_differences(argv[1], argv[2]);
         return 0;
         
